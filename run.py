acc11489a28c2e6d25f02bcae6cec7a5791e0126
Kevin Axel Mejia Hernandez <Al222410994@gmail.com>
2026-01-10 08:27:23 -0600

    Configurar app para Render

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from todor import create_app
+import os
+from todor import app  # importa tu app desde el paquete todor
 
-app = create_app()
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
